d9d484e9f7a510965a375b6f3173543b3dfae2f0
kiptoo <kiptooharon.hk@gmail.com>
2023-07-28 02:52:27 +0300

    added dec to bits conversion for handling %p

---
+++ dec_to_bitc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* identifier_b - bit conversion
+* @buffer: fflush buffer
+* @list: variable arg list
+* Return: 1 -sucess 0 -failure
+*/
+int identifier_b(char *buffer, va_list list)
+{
+	unsigned int i = 0;
+	long int number = va_arg(list, unsigned int);
+	char *bit_space = malloc(sizeof(char) * 64);
+
+	if (bit_space == NULL)
+		return (0);
+
+	while (number != 0)
+	{
+		bit_space[i] = number % 2 + '0';
+		number = number / 2;
+		i++;
+	}
+
+	bit_space[i] = '\0';
+	rev_string(bit_space, i);
+	_strcat(buffer, bit_space);
+
+	return (1);
+}
