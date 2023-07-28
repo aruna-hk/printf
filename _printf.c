3bd493541c4b3f9766a5cbb81e03304fd463a314
kiptoo <kiptooharon.hk@gmail.com>
2023-07-28 20:43:37 +0300

    continue --statement

--- _printf.c
+++ _printf.c
@@ -27,9 +27,9 @@ void append_to_buffer(char *buffer_s, va_list ss, char p)
 	if (func_ptr == NULL)
 	{
 		_strcat(buffer_s, "%");
+		continue;
 	}
-	else
-		func_ptr(buffer_s, ss);
+	func_ptr(buffer_s, ss);
 }
 /**
 * _printf - printf formatted data to standard output
